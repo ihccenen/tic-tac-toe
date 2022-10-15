e9f6ffc209f5c88c18082e57442f62c426868a5d
ihccenen <wirkz01@gmail.com>
2022-10-15 18:51:00 -0300

    Added restart

--- script/script.js
+++ script/script.js
@@ -11,7 +11,23 @@ const gameBoard = (() => {
     [2, 4, 6],
   ]
 
-  return { board, rowCombinations }
+  const restart = () => {
+    board.splice(0)
+    rowCombinations.splice(
+      0,
+      8,
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    )
+  }
+
+  return { board, rowCombinations, restart }
 })()
 
 const gameFlow = (() => {
