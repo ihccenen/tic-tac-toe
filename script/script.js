ec2237301dd1873dbdcb9dc1c7ae1364cbe270db
ihccenen <wirkz01@gmail.com>
2022-10-15 18:42:56 -0300

    Added rowCombinations

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
 const gameBoard = (() => {
   const board = []
+  const rowCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  return { board, rowCombinations }
 })()
 
 const createPlayer = (name, char) => {
-  const player = {
+  const _player = {
     name,
     char,
   }
